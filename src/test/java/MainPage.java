4b89f04a2209afec1997f2256bf976b73f798526
Palvic12 <38190539+Palvic12@users.noreply.github.com>
2021-02-18 23:36:14 -0800

    HomeTask1

---
+++ src/test/java/MainPage.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class MainPage {
+    String mainUrl = "https://romanceabroad.com/";
+
+    @Test
+    public void testMainPage(){
+
+        WebDriver driver;
+        System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(mainUrl);
+        driver.quit();
+    }
+    @Test
+    public void testMainPage2(){
+
+        WebDriver driver;
+        System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(mainUrl);
+        driver.quit();
+    }
+
+}
